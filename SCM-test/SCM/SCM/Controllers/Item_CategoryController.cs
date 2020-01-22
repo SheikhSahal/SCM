0ef05a6eefee955eceffffb600ca2dbc04284a6c
Sheikh_sahal <s.m.sahal786@outlook.com>
2020-01-23 04:22:21 +0500

    SCM

---
+++ SCM-test/SCM/SCM/Controllers/Item_CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCM.Controllers
+{
+    public class Item_CategoryController : Controller
+    {
+        // GET: Item_Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
